ec5d496e298271edbc375e3d0dc568087b5cbc95
Andi Zahrina Athirah Ahmad <117283292+2209106126-azahrinathirah@users.noreply.github.com>
2024-09-16 19:48:26 +0800

    Posttest 1

---
+++ WebsiteBakeryShop/WebsiteBakeryShop/FlourinBakery/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home/index.html')
+
+def about(request):
+    return render(request, 'home/about.html')
+
+def products(request):
+    return render(request, 'home/products.html')
+
+def myOrders(request):
+    return render(request, 'home/myOrders.html')
+
+def contact(request):
+    return render(request, 'home/contact.html')
